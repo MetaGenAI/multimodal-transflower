e8095c5cc76abff407ce3d3ce3e8eff587fc5b6c
Guillermo Valle <guillefix@gmail.com>
2021-06-22 20:22:22 +0200

    aaaaa

--- meta_script.sh
+++ meta_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #for exp in moglow_expmap1
-#for exp in transflower_expmap_cr4_label_bs5
-for exp in transflower_expmap_cr_label2
+for exp in moglow_expmap1_tf
+#for exp in transflower_expmap_cr4_label_bs5c
+#for exp in transflower_expmap_cr_label2
 do
 	#sbatch slurm_script.slurm $exp --experiment_name ${exp}_newdata4 --num_nodes 8 --continue_train --no_load_hparams 
 	#sbatch slurm_script.slurm $exp --experiment_name ${exp}_newdata2 --num_nodes 8 --continue_train
@@ -14,7 +15,8 @@ do
 	#sbatch slurm_script2b.slurm $exp --experiment_name ${exp}_newdata2 --num_nodes 1
 	#sbatch slurm_script2b.slurm $exp --experiment_name ${exp}_newdata --num_nodes 1 --continue_train 
 	#sbatch slurm_script2b.slurm $exp --experiment_name ${exp}_newdata --num_nodes 1 --continue_train --base_filenames_file base_filenames_train_nojd.txt
-	sbatch slurm_script1.slurm $exp --experiment_name ${exp}_newdata --num_nodes 1 #--fix_lengths
+	#sbatch slurm_script1.slurm $exp --experiment_name ${exp}_newdata --num_nodes 1 #--fix_lengths
+	sbatch slurm_script1.slurm $exp --experiment_name ${exp}_newdata2 --num_nodes 1
 	#sbatch slurm_script4.slurm $exp --experiment_name ${exp}_newdata --num_nodes 1
 
 	#sbatch slurm_script2.slurm $exp --experiment_name=${exp}_smoldata --data_dir=${SCRATCH}/data/dance_combined --base_filenames_file base_filenames_train_finetune.txt
