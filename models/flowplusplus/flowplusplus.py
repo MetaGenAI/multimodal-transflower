d99ffa4faf01f9cc4fd3dc9059665e97ecdf58a0
Guillermo Valle <guillefix@gmail.com>
2022-01-14 14:39:41 +0100

    some flowplusplus stuff?

--- models/flowplusplus/flowplusplus.py
+++ models/flowplusplus/flowplusplus.py
@@ -301,3 +301,173 @@ class _FlowStep(nn.Module):
         # print(x.std())
         return x, sldj
         
+class _FlowStepTesting(nn.Module):
+    """Recursive builder for a Flow++ model.
+
+    Each `_FlowStep` corresponds to a single scale in Flow++.
+    The constructor is recursively called to build a full model.
+
+    Args:
+        scales (tuple): Number of each type of coupling layer in each scale.
+            Each scale is a 2-tuple of the form (num_channelwise, num_checkerboard).
+        in_channels (int): Number of channels in the input.
+        mid_channels (int): Number of channels in the intermediate layers.
+        num_blocks (int): Number of residual blocks in the s and t network of
+            `Coupling` layers.
+        num_components (int): Number of components in the mixture.
+        use_attn (bool): Use attention in the coupling layers.
+        drop_prob (float): Dropout probability.
+    """
+    def __init__(self, scales, in_shape, cond_dim, mid_channels, num_blocks, num_components, use_attn, use_logmix, use_transformer_nn, use_pos_emb, use_rel_pos_emb, num_heads, drop_prob, norm_layer, bn_momentum, cond_concat_dims, cond_seq_len):
+        super(_FlowStep, self).__init__()
+        in_channels, in_height, in_width = in_shape
+        num_channelwise, num_checkerboard = scales[0]
+        #import pdb;pdb.set_trace()
+        channels = []
+        for i in range(num_channelwise):
+            new_channels = in_channels// 2
+            out_channels = in_channels-new_channels
+            # print(norm_layer)
+            if norm_layer == "batchnorm":
+                channels += [BatchNorm(in_channels, bn_momentum)]
+            elif norm_layer == "actnorm":
+                channels += [ActNorm(in_channels)]
+            if cond_concat_dims:
+                c_in_channels = new_channels + cond_dim
+                seq_length = in_height
+            else:
+                c_in_channels = new_channels
+                seq_length = in_height + cond_seq_len
+            channels += [InvertibleConv1x1(in_channels)]
+            channels += [Coupling(in_channels=c_in_channels,
+                                  cond_dim=cond_dim,
+                                  out_channels=out_channels,
+                                  mid_channels=mid_channels,
+                                  num_blocks=num_blocks,
+                                  num_components=num_components,
+                                  use_attn=use_attn,
+                                  use_logmix=use_logmix,
+                                  use_transformer_nn=use_transformer_nn,
+                                  use_pos_emb=use_pos_emb,
+                                  use_rel_pos_emb=use_rel_pos_emb,
+                                  num_heads=num_heads,
+                                  seq_length=seq_length,
+                                  output_length=in_height,
+                                  concat_dims=cond_concat_dims,
+                                  drop_prob=drop_prob)]#,
+                         #Flip()] Flip currently does not work with odd number of channels. But is it needed when we have channel mixing with 1x1convs? 
+
+        checkers = []
+        if cond_concat_dims:
+            c_in_channels = new_channels + cond_dim
+            seq_length = in_height
+        else:
+            c_in_channels = new_channels
+            seq_length = in_height + cond_seq_len
+        for i in range(num_checkerboard):
+            if norm_layer == "batchnorm":
+                checkers += [BatchNorm(in_channels, bn_momentum)]
+            elif norm_layer == "actnorm":
+                checkers += [ActNorm(in_channels)]
+            checkers += [InvertibleConv1x1(in_channels)]
+            checkers += [Coupling(in_channels=c_in_channels,
+                                  out_channels=in_channels,
+                                  mid_channels=mid_channels,
+                                  num_blocks=num_blocks,
+                                  num_components=num_components,
+                                  use_attn=use_attn,
+                                  use_logmix=use_logmix,
+                                  use_transformer_nn=use_transformer_nn,
+                                  use_pos_emb=use_pos_emb,
+                                  use_rel_pos_emb=use_rel_pos_emb,
+                                  num_heads=num_heads,
+                                  seq_length=seq_length,
+                                  output_length=in_height,
+                                  concat_dims=cond_concat_dims,
+                                  drop_prob=drop_prob)]#,
+                         #Flip()]
+        self.channels = nn.ModuleList(channels) if channels else None
+        self.checkers = nn.ModuleList(checkers) if checkers else None
+
+        if len(scales) <= 1:
+            self.next = None
+        else:
+            next_shape = (in_channels, in_height // 2, in_width)
+            self.next = _FlowStep(scales=scales[1:],
+                                  in_shape=next_shape,
+                                  cond_dim=2*cond_dim,
+                                  mid_channels=mid_channels,
+                                  num_blocks=num_blocks,
+                                  num_components=num_components,
+                                  use_attn=use_attn,
+                                  use_logmix=use_logmix,
+                                  use_transformer_nn=use_transformer_nn,
+                                  use_pos_emb=use_pos_emb,
+                                  use_rel_pos_emb=use_rel_pos_emb,
+                                  num_heads=num_heads,
+                                  norm_layer = norm_layer,
+                                  bn_momentum = bn_momentum,
+                                  cond_concat_dims = cond_concat_dims,
+                                  cond_seq_len = cond_seq_len,
+                                  drop_prob=drop_prob)
+                                  
+        self.z_shape = (in_channels, in_height, in_width)
+        
+    def z_dim(self):
+        return self.z_shape
+
+    def forward(self, x, cond, sldj):#, reverse=False):
+            
+        reverse=True
+        if reverse:
+            #import pdb;pdb.set_trace()
+            if self.next is not None:
+                x = squeeze(x)
+                cond = squeeze(cond)
+                x, x_split = x.chunk(2, dim=1)
+                x, sldj = self.next(x, cond, sldj, reverse)
+                x = torch.cat((x, x_split), dim=1)
+                x = unsqueeze(x)
+                cond = unsqueeze(cond)
+
+            if self.checkers:
+                x = checkerboard(x)
+                for flow in reversed(self.checkers):
+                    x, sldj = flow(x, cond, sldj, reverse)
+                x = checkerboard(x, reverse=True)
+
+            if self.channels:
+                x = channelwise(x)
+                for flow in reversed(self.channels):
+                    x, sldj = flow(x, cond, sldj, reverse)
+                x = channelwise(x, reverse=True)
+        else:
+            # import pdb;pdb.set_trace()
+            if self.channels:
+                x = channelwise(x)
+                for flow in self.channels:
+                    # import pdb;pdb.set_trace()
+                    x, sldj = flow(x, cond, sldj, reverse)
+                    # print(type(flow).__name__)
+                    # print(x[0].std())
+                x = channelwise(x, reverse=True)
+
+            if self.checkers:
+                x = checkerboard(x)
+                for flow in self.checkers:
+                    x, sldj = flow(x, cond, sldj, reverse)
+                x = checkerboard(x, reverse=True)
+
+            if self.next is not None:
+                # import pdb;pdb.set_trace()
+                # here we apply the flow steps but only to dimensions sampled at a lower scale. Hmm feels a bit weird
+                x = squeeze(x)
+                cond = squeeze(cond)
+                x, x_split = x.chunk(2, dim=1)
+                x, sldj = self.next(x, cond, sldj, reverse)
+                x = torch.cat((x, x_split), dim=1)
+                x = unsqueeze(x)
+
+        # print(x.std())
+        return x, sldj
+        
