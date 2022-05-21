62e51c515de0912c1128c06e0dee9b146e8e8180
Guillermo Valle <guillefix@gmail.com>
2022-05-21 02:54:56 +0200

    feat ext

--- feature_extraction/process_tw_data.sh
+++ feature_extraction/process_tw_data.sh
@@ -21,10 +21,10 @@ mpirun="mpirun --use-hwthread-cpus"
 #find $1 -exec rename -f 's/npz.acts.npy.annotation/annotation/' {} +
 
 #THIS
-$py feature_extraction/extract_transform2.py $1 --feature_name npz.acts --transforms scaler
-$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name npz.acts --transform_name scaler --new_feature_name npz.acts_scaled
-$py feature_extraction/extract_transform2.py $1 --feature_name npz.obs_cont --transforms scaler
-$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name npz.obs_cont --transform_name scaler --new_feature_name obs_cont_scaled
+#$py feature_extraction/extract_transform2.py $1 --feature_name npz.acts --transforms scaler
+#$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name npz.acts --transform_name scaler --new_feature_name npz.acts_scaled
+#$py feature_extraction/extract_transform2.py $1 --feature_name npz.obs_cont --transforms scaler
+#$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name npz.obs_cont --transform_name scaler --new_feature_name obs_cont_scaled
 #
 
 #$py feature_extraction/extract_transform2.py $1 --feature_name obs_cont_single_nocol --transforms scaler
@@ -37,15 +37,19 @@ $mpirun $py feature_extraction/apply_transforms.py $@ --feature_name npz.obs_con
 
 $mpirun $py ./feature_extraction/combine_feats.py $@ $1/base_filenames.txt --feature_names npz.times_to_go,npz.times_to_go --new_feature_name duplicated_times_to_go
 
+#$py feature_extraction/extract_transform2.py $1 --feature_name npz.times_to_go --transforms scaler
+#$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name npz.times_to_go --transform_name scaler --new_feature_name times_to_go_scaled
+$py feature_extraction/extract_transform2.py $1 --feature_name duplicated_times_to_go --transforms scaler
+$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name duplicated_times_to_go --transform_name scaler --new_feature_name duplicated_times_to_go_scaled
 
 #$py feature_extraction/trim_seqs.py $1 --feature_name obs_cont_single_nocol_noarm --trim_begin 10 --new_feature_name obs_cont_single_nocol_noarm_trim
 #$py feature_extraction/extract_transform2.py $1 --feature_name obs_cont_single_nocol_noarm_trim --transforms scaler
 #$py feature_extraction/extract_transform2.py $1 --feature_name obs_cont_single_nocol_noarm_incsize --transforms scaler
 #$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name obs_cont_single_nocol_noarm_incsize --transform_name scaler --new_feature_name obs_cont_single_nocol_noarm_incsize_scaled
 
-$mpirun $py feature_extraction/trim_seqs.py $@ --feature_name obs_cont_single_nocol_noarm_incsize --trim_begin 25 --new_feature_name obs_cont_single_nocol_noarm_incsize_trim
-$py feature_extraction/extract_transform2.py $@ --feature_name obs_cont_single_nocol_noarm_incsize_trim --transforms scaler
-$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name obs_cont_single_nocol_noarm_incsize_trim --transform_name scaler --new_feature_name obs_cont_single_nocol_noarm_incsize_trim_scaled
+#$mpirun $py feature_extraction/trim_seqs.py $@ --feature_name obs_cont_single_nocol_noarm_incsize --trim_begin 25 --new_feature_name obs_cont_single_nocol_noarm_incsize_trim
+#$py feature_extraction/extract_transform2.py $@ --feature_name obs_cont_single_nocol_noarm_incsize_trim --transforms scaler
+#$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name obs_cont_single_nocol_noarm_incsize_trim --transform_name scaler --new_feature_name obs_cont_single_nocol_noarm_incsize_trim_scaled
 
 #$mpirun $py feature_extraction/apply_transforms.py $@ --feature_name obs_cont_single_nocol_noarm_trim --transform_name scaler --new_feature_name obs_cont_single_nocol_noarm_trim_scaled
 #$py feature_extraction/trim_seqs.py $1 --feature_name npz.acts --trim_begin 10 --new_feature_name acts_trim
