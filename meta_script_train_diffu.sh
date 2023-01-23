a45077512e8f1f6fd5e1c5f44afda370d97625cc
Guillermo Valle <guillefix@gmail.com>
2023-01-23 22:42:12 +0000

    new train scripts :3

---
+++ meta_script_train_diffu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#./script_train_diff.sh transfusion_base4_lessN2 --base_filenames_file base_filenames_train_filtered_smol2.txt --max_epochs 500
+./script_train_diff.sh transfusion_base4_lessN2 --base_filenames_file base_filenames_train_filtered2_shuf.txt --max_epochs 200
