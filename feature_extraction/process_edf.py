563fba767f670e625b4a7ba837866f4fb7a05dc9
guillefix3 <guillefix@gmail.com>
2023-06-21 00:18:52 +0100

    basic edf processing files

---
+++ feature_extraction/process_edf.py
@@ -0,0 +1,56 @@
+import numpy as np
+# import librosa
+from pathlib import Path
+import json
+import os.path
+import sys
+import argparse
+import pickle
+import torch
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(THIS_DIR, os.pardir))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+sys.path.append(ROOT_DIR)
+from utils import distribute_tasks
+
+from analysis.pymo.parsers import BVHParser
+from analysis.pymo.data import Joint, MocapData
+from analysis.pymo.preprocessing import *
+from sklearn.pipeline import Pipeline
+import joblib as jl
+
+parser = argparse.ArgumentParser(description="Preprocess motion data")
+
+parser.add_argument("data_path", type=str, help="Directory contining EDF records")
+parser.add_argument("--replace_existing", action="store_true")
+# parser.add_argument("--param", type=str, default="expmap", help="expmap, position")
+# parser.add_argument("--do_mirror", action="store_true", help="whether to augment the data with mirrored motion")
+# parser.add_argument("--fps", type=int, default=60)
+
+args = parser.parse_args()
+
+# makes arugments into global variables of the same name, used later in the code
+globals().update(vars(args))
+data_path = Path(data_path)
+
+## distributing tasks accross nodes ##
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+print(rank)
+
+candidate_edf_folders = sorted(data_path.glob('*'), key=lambda path: path.parent.__str__())
+#candidate_motion_files = candidate_motion_files[:32]
+tasks = distribute_tasks(candidate_edf_folders,rank,size)
+
+from utils.edf_motion_utils import extract_features # only support motion for now
+
+for task in tasks:
+    folder = task
+    features = extract_features(folder)
+    features_file = folder + "/motion_features.npy"
+    if replace_existing or not os.path.isfile(features_file):
+        np.save(features_file, features)
+        print("Saved features to {}".format(features_file))
---
+++ feature_extraction/utils/edf_motion_utils.py
@@ -0,0 +1,84 @@
+import json
+# import schema
+import asyncio
+from websockets.server import serve
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter, BinaryEncoder, BinaryDecoder
+from io import BytesIO
+from avro.utils import randbytes
+from avro.datafile import MAGIC, META_SCHEMA
+import time
+from pymo.rotation_tools import Rotation, euler2expmap, euler2expmap2, expmap2euler, euler_reorder, unroll
+from pymo.Quaternions import Quaternions
+
+NODE_TYPES_LIST = ["Root", "Head", "LeftHand", "RightHand"]
+from constants import ROT_ORDER
+
+import numpy as np
+
+
+def read_data(data_file):
+    reader = DataFileReader(open(data_file, "rb"), DatumReader())
+
+    # Encode schema as binary and send it as the first message
+    schema_buffer = BytesIO()
+    data_buffer = BytesIO()
+    encoder = BinaryEncoder(data_buffer)
+    schema = avro.schema.parse(reader.schema)
+
+    yield schema
+
+    writer = DatumWriter(schema)
+
+    # Send binary-encoded data
+    # t0 = time.time()
+    t0b = time.time()
+    initial_time = None
+    for i, datum in enumerate(reader):
+        yield datum
+        # print("sent data")
+    reader.close()
+
+def extract_features(path):
+    print(path)
+    fast_data_file = path+"/fast_data.avro"
+    slow_data_file = path+"/slow_data.avro"
+    fast_data = read_data(fast_data_file)
+    slow_data = read_data(slow_data_file)
+
+    fast_schema = next(fast_data)
+    slow_schema = next(slow_data)
+
+    slow_datum = next(slow_data)
+
+    types_in_data = [n["nodeType"] for n in slow_datum["selfBio"][0]["motion"]["nodes"]]
+
+    node_indices = [types_in_data.index(t) for t in NODE_TYPES_LIST]
+
+    fast_data_list = list(read_data(fast_data_file))[1:]
+    positions = np.array([[x["position"] for x in fast_datum["selfBio"][0]["motion"]["nodes"]] for fast_datum in fast_data_list])
+    rotations = np.array([[x["rotation"] for x in fast_datum["selfBio"][0]["motion"]["nodes"]] for fast_datum in fast_data_list])
+
+    q = Quaternions(rotations)
+    eulers = q.euler(order=ROT_ORDER)
+
+    exps = [unroll(np.array([euler2expmap(f, ROT_ORDER, True) for f in eulers[:,i,:]])) for i in range(eulers.shape[1])]
+
+    rots = np.stack([exps[i] for i in node_indices], axis=1)
+
+    rots_flat = rots.reshape(rots.shape[0], -1)
+
+    poss_flat = positions.reshape(positions.shape[0], -1)
+
+    motion_features = np.concatenate([rots_flat, poss_flat], axis=1)
+
+    return motion_features
+
+if __name__ == "__main__":
+
+    features = extract_features(path)
+
+    features_file = ""
+
+    np.save(features_file, features)
