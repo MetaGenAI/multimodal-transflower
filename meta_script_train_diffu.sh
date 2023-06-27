da4658a74be05521c4f0f3620a952150ab8e4eeb
Guillermo Valle <guillefix@gmail.com>
2023-06-27 11:01:05 +0100

    edf scripts

--- meta_script_train_diffu.sh
+++ meta_script_train_diffu.sh
@@ -15,6 +15,6 @@
 ## edf gesture data
 
 #exp=transfusion_baseA2
-exp=transfusion_baseA
-./script_train_diffu.sh $exp --hparams_file=training/hparams/edf_gestures/${exp}.yaml --experiment_name ${exp}_edf_gestures1_nodp_longer_deeper --num_nodes 1 --max_epochs 80 --data_dir=./data/edf_extracted_data/ --base_filenames_file base_filenames.txt $@
+exp=transfusion_baseA2
+./script_train_diffu.sh $exp --hparams_file=training/hparams/edf_gestures/${exp}.yaml --experiment_name ${exp}_edf_gestures1_nodp_longer_deeper --num_nodes 1 --max_epochs 160 --data_dir=./data/edf_extracted_data_rel/ --base_filenames_file base_filenames.txt $@
 #./script_train_diffu.sh $exp --hparams_file=training/hparams/edf_gestures/${exp}.yaml --experiment_name ${exp}_edf_gestures2 --num_nodes 1 --max_epochs 80 --data_dir=./data/edf_extracted_data2/ --base_filenames_file base_filenames.txt $@
