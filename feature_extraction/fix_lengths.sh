3fe4aa438d42d6ced0117fb8454249a06c96e72d
Guillermo Valle <guillefix@gmail.com>
2023-07-10 14:16:33 +0100

    fixed transflower hparams file

--- feature_extraction/fix_lengths.sh
+++ feature_extraction/fix_lengths.sh
@@ -3,8 +3,8 @@ folder=$1
 py=python3
 #py=python3.8
 #py=python
-#n=$(nproc)
-n=6
+n=$(nproc)
+#n=6
 
 #mpirun -n $n $py feature_extraction/fix_lengths.py $1 $1/base_filenames.txt --fix_length_types none,end,end --modalities dance_style,expmap_cr_scaled_20,audio_feats_scaled_20
 #$py feature_extraction/fix_lengths.py $1 $1/base_filenames.txt --fix_length_types none,end,end --modalities dance_style,expmap_cr_scaled_20,audio_feats_scaled_20
@@ -14,4 +14,5 @@ n=6
 #mpirun -n $n $py feature_extraction/fix_lengths.py $1 $1/base_filenames.txt --fix_length_types end,end --modalities speech.wav_envelope_scaled,motion_features_scaled1
 #$py feature_extraction/fix_lengths.py $1 $1/base_filenames_train.txt --fix_length_types end,end --modalities expmap_scaled_20,audio_feats_scaled_20
 #$py feature_extraction/fix_lengths.py $1 $1/base_filenames.txt --fix_length_types end,end --modalities ogg_envelope_scaled,rel_feats_scaled1
-mpirun -n $n $py feature_extraction/fix_lengths.py $1 $1/base_filenames.txt --fix_length_types end,end --modalities ${@:2}
+#mpirun -n $n $py feature_extraction/fix_lengths.py $1 $1/base_filenames.txt --fix_length_types end,end --modalities ${@:2}
+mpirun -n $n $py feature_extraction/fix_lengths.py $1 $1/base_filenames_train.txt --fix_length_types end,end --modalities ${@:2}
