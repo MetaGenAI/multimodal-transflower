f9f791c9bdcfdc4dcdbfd09bf9540772a287ad75
Guillermo Valle <guillefix@gmail.com>
2023-07-13 00:46:28 +0100

    added losses function

--- models/diffusion/gaussian_diffusion.py
+++ models/diffusion/gaussian_diffusion.py
@@ -1322,10 +1322,93 @@ class GaussianDiffusion:
         output = th.where((t == 0), decoder_nll, kl)
         return {"output": output, "pred_xstart": out["pred_xstart"]}
 
+    # def training_losses(
+    #     self, model, x_start, t, sparse=None, model_kwargs=None, noise=None, dataset=None
+    # ):
+    #     pass
+
+    def masked_l2(self, a, b):
+        bs, n, c = a.shape
+
+        loss = torch.mean(
+            torch.norm(
+                (a - b).reshape(-1, 6),
+                2,
+                1,
+            )
+        )
+
+        return loss
+
     def training_losses(
         self, model, x_start, t, sparse=None, model_kwargs=None, noise=None, dataset=None
     ):
-        pass
+
+        if model_kwargs is None:
+            model_kwargs = {}
+        if noise is None:
+            noise = th.randn_like(x_start)
+        x_t = self.q_sample(x_start, t, noise=noise)
+
+        terms = {}
+
+        if self.loss_type == LossType.KL or self.loss_type == LossType.RESCALED_KL:
+            terms["loss"] = self._vb_terms_bpd(
+                model=model,
+                x_start=x_start,
+                x_t=x_t,
+                t=t,
+                clip_denoised=False,
+                model_kwargs=model_kwargs,
+            )["output"]
+            if self.loss_type == LossType.RESCALED_KL:
+                terms["loss"] *= self.num_timesteps
+        elif self.loss_type == LossType.MSE or self.loss_type == LossType.RESCALED_MSE:
+            model_output = model(x_t, self._scale_timesteps(t), sparse, **model_kwargs)
+
+            if self.model_var_type in [
+                ModelVarType.LEARNED,
+                ModelVarType.LEARNED_RANGE,
+            ]:
+                B, C = x_t.shape[:2]
+                assert model_output.shape == (B, C * 2, *x_t.shape[2:])
+                model_output, model_var_values = th.split(model_output, C, dim=1)
+                # Learn the variance using the variational bound, but don't let
+                # it affect our mean prediction.
+                frozen_out = th.cat([model_output.detach(), model_var_values], dim=1)
+                terms["vb"] = self._vb_terms_bpd(
+                    model=lambda *args, r=frozen_out: r,
+                    x_start=x_start,
+                    x_t=x_t,
+                    t=t,
+                    clip_denoised=False,
+                )["output"]
+                if self.loss_type == LossType.RESCALED_MSE:
+                    # Divide by 1000 for equivalence with initial implementation.
+                    # Without a factor of 1/1000, the VB term hurts the MSE term.
+                    terms["vb"] *= self.num_timesteps / 1000.0
+
+            target = {
+                ModelMeanType.PREVIOUS_X: self.q_posterior_mean_variance(
+                    x_start=x_start, x_t=x_t, t=t
+                )[0],
+                ModelMeanType.START_X: x_start,
+                ModelMeanType.EPSILON: noise,
+            }[self.model_mean_type]
+
+            assert model_output.shape == target.shape == x_start.shape
+
+            terms["rot_mse"] = self.masked_l2(
+                target,
+                model_output,
+            )
+
+            terms["loss"] = terms["rot_mse"] + terms.get("vb", 0.0)
+
+        else:
+            raise NotImplementedError(self.loss_type)
+
+        return terms
 
     def _prior_bpd(self, x_start):
         """
